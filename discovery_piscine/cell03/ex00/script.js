2920184d8e6219d33efc5033cfbc86b4c141e2a2
Adeline GOUET <agouet@e1r11p20.42.fr>
2022-04-29 17:07:06 +0200

    sujets

--- discovery_piscine/cell03/ex00/script.js
+++ discovery_piscine/cell03/ex00/script.js
@@ -1,13 +1,10 @@
-const bouton2 = document.getElementsByTagName('button');
-
-var colors = ['#ff0000', '#00ff00', '#0000ff'];
-var random_color = colors[Math.floor(Math.random() * colors.length)];
-document.getElementById('button').style.color = random_color;
 
 
-bouton2.onclick=function(){change_background()};
 
-function change_background()
-{	
-	document.body.style.backgroundColor= random_color;
-}
+let bouton = document.querySelector('button');
+bouton.addEventListener("click", displayDate);
+	//document.body.style.backgroundColor= "pink";
+//
+
+//var colors = ['#ff0000', '#00ff00', '#0000ff'];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
