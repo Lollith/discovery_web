f84e8dcbbfb116b560e3311aa4a477fbefd0ae7d
agouet <adeline.gouet@gmail.com>
2022-10-27 12:16:39 +0200

    ceel3

--- discovery_piscine/cell03/ex00/script.js
+++ discovery_piscine/cell03/ex00/script.js
@@ -1,10 +1,16 @@
 
 
 
-let bouton = document.querySelector('button');
-bouton.addEventListener("click", displayDate);
-	//document.body.style.backgroundColor= "pink";
-//
+// const boutton1 = document.querySelector('.boutton1'); // recup mon objet boutton
+// boutton1.addEventListener('click', changecolor);// ecoute pour recup un event
 
-//var colors = ['#ff0000', '#00ff00', '#0000ff'];
-//var random_color = colors[Math.floor(Math.random() * colors.length)];
+const boutton2 = document.querySelector('button'); // recup mon objet boutton
+
+boutton2.onclick = function() {changecolor()};
+
+function changecolor(){
+		document.body.style.background = random_color;
+}
+
+var colors = ['#ff0000', '#00ff00', '#0000ff'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
