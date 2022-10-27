54c89c74036f4d1c2636cba3b68bbff6b555ad56
agouet <adeline.gouet@gmail.com>
2022-10-27 14:04:15 +0200

    cell2

--- discovery_piscine/cell03/ex00/script.js
+++ discovery_piscine/cell03/ex00/script.js
@@ -1,16 +1,15 @@
 
 
 
-// const boutton1 = document.querySelector('.boutton1'); // recup mon objet boutton
-// boutton1.addEventListener('click', changecolor);// ecoute pour recup un event
+const boutton1 = document.querySelector('.boutton1'); // recup mon objet boutton
+boutton1.addEventListener('click', changecolor);// ecoute pour recup un event
 
-const boutton2 = document.querySelector('button'); // recup mon objet boutton
+// const boutton2 = document.querySelector('button'); // recup mon objet boutton
+// boutton2.onclick = function() {changecolor()};
 
-boutton2.onclick = function() {changecolor()};
-
-function changecolor(){
-		document.body.style.background = random_color;
+function changecolor() {
+	var colors = ['#ff0000', '#00ff00', '#0000ff'];
+    document.body.style.background = colors[Math.floor(Math.random() * colors.length)];
 }
 
-var colors = ['#ff0000', '#00ff00', '#0000ff'];
-var random_color = colors[Math.floor(Math.random() * colors.length)];
+
